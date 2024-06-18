5e9176b0e6f6b7d338962c11b20c3ac4890edd53
Son Tran <286.trants@gmail.com>
2024-06-18 14:56:44 +0700

    chore(docs): update documentation and release v1.0.0 version * Update change log
    * Add version.go

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+# Changelog
+
+## v1.0.0
+
+### Features
+* feat(multierr): implement multierr package
+* feat(codes): implement codes package
+
+### Enhancements
+* chore(docs): add changelog categories and update documentation
---
+++ version.go
@@ -0,0 +1,8 @@
+// Copyright (c) 2022-2024 Focela Technologies, All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package multierr
+
+// Version is the current version.
+const Version = "v1.0.0"
